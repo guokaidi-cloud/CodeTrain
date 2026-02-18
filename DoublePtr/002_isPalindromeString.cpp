ffcd956e783c9582e86bcf57d48e47793c0c37a1
guokaidi-cloud <414031548@qq.com>
2026-02-18 23:29:46 +0800

    is palindrome str

---
+++ DoublePtr/002_isPalindromeString.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  /**
+   * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+   *
+   *
+   * @param str string字符串 待判断的字符串
+   * @return bool布尔型
+   */
+  bool judge(string str) {
+    // write code here
+
+    if (str.size() == 0)
+      return true;
+
+    int l = 0, r = str.size() - 1;
+    while (l < r) {
+      if (str[l] != str[r]) {
+        return false;
+      }
+      ++l, --r;
+    }
+    return true;
+  }
+};
